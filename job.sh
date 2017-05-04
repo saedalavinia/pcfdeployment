a4a9f2387a4260b25b9e5b4091b69fef9aa31c70
Saed Alavinia <saedalav@gmail.com>
2017-05-04 15:32:09 -0400

    Second Commit

--- job.sh
+++ job.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-ssh-agent bash
-ssh-add ~/.ssh/us-east-1.pem
+cd /home/saedalav/Documents/PCF
 
-ansible-playbook deployOpsman.yml
+source /home/saedalav/.bashrc
+#eval 'ssh-agent'
+#ssh-add /home/saedalav/.ssh/us-east-1.pem
+
+echo "Starting Ansible Commands"
+echo "-------------------------"
+
+ansible-playbook deployAll.yml -vvv
+
+echo "Ansible Command Completed"
+echo "-------------------------"
+
+echo "Sending Email:"
+echo "-------------------------"
+
+cat /home/saedalav/Desktop/output.txt | mail -s "Ansible Output Logs" saedalav@gmail.com
+
+
+echo "Job Finished Successfully"
+
