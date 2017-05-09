0c2f091f50b2e1b13430f325e0b6cbb79dc19224
Saed Alavinia <saedalav@gmail.com>
2017-05-09 15:06:38 +0000

    Added a Branch that deploys a more cost efficient PCF

---
+++ costefficientjob.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+email=saedalav@gmail.com
+output_path=/home/ec2-user/output.txt
+root_directory=/home/ec2-user/pcfdeployment
+
+cd $root_directory
+
+source /home/ec2-user/.bashrc
+
+echo "Pulling the latest updates from Git"
+echo "-----------------------------------"
+
+git checkout costefficient
+git pull origin costefficient
+
+
+echo "Starting Ansible Commands"
+echo "-------------------------"
+
+ansible-playbook deployAll.yml -vvv
+
+
+echo "Ansible Command Completed"
+echo "-------------------------"
+
+echo "Sending Email:"
+echo "-------------------------"
+
+cat $output_path | mail -s "Ansible Output Logs" $email
+
+
+echo "Job Finished Successfully"
+
